612d7b7e52802980dd20683a17a76752778de453
7Gabriel <joao1.gabriel@hotmail.com>
2017-12-06 11:06:37 -0200

    Atualizacao dos arquivos de SQL

--- src/main/java/br/com/pontointeligente/api/PontoInteligenteApiApplication.java
+++ src/main/java/br/com/pontointeligente/api/PontoInteligenteApiApplication.java
@@ -3,6 +3,7 @@ package br.com.pontointeligente.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PontoInteligenteApiApplication {
 
